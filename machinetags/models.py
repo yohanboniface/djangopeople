c8b5b5ef1810cf1dbe25175ca259889c2b1aa372
Bruno Renié <buburno@gmail.com>
2011-04-03 15:25:48 +0200

    Removed the defaultdict implementation from machinetags.utils
    Also pep8/pyflake'd the machinetags app.

--- machinetags/models.py
+++ machinetags/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
+
 class MachineTaggedItem(models.Model):
     "A machine tag on an item."
     namespace = models.CharField(max_length=50, db_index=True)
     predicate = models.CharField(max_length=50, db_index=True)
     value = models.CharField(max_length=255, db_index=True)
-    
+
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
 
@@ -26,18 +27,21 @@ import re
 _part_re = re.compile('^[a-z][a-z0-9_]*$')
 _machinetag_re = re.compile('^([a-z][a-z0-9_]*):([a-z][a-z0-9_]*)=(.*)$')
 
+
 def is_valid_part(part):
     "Checks string is a valid namespace or predicate"
     return bool(_part_re.match(part))
 
+
 def parse_machinetag(namespace_or_fulltag, predicate=None, value=None):
     if predicate:
-        assert value, 'If you provide a predicate you must also provide a value'
+        assert value, ('If you provide a predicate you must '
+                       'also provide a value')
         assert is_valid_part(namespace_or_fulltag), 'namespace must be valid'
         assert is_valid_part(predicate), 'predicate must be valid'
         namespace = namespace_or_fulltag
     else:
-        match = _machinetag_re.match(machinetag)
+        match = _machinetag_re.match(namespace_or_fulltag)
         assert match, 'machinetag must be of format namespace:predicate=value'
         namespace, predicate, value = match.groups()
         if value[0] == '"' or value[-1] == '"':
@@ -47,36 +51,40 @@ def parse_machinetag(namespace_or_fulltag, predicate=None, value=None):
             value = value.replace(r'\"', '"')
     return namespace, predicate, value
 
+
 def tag_exists(*args):
     namespace, predicate, value = parse_machinetag(*args)
     return MachineTaggedItem.objects.filter(
-        namespace = namespace,
-        predicate = predicate,
-        value = value
+        namespace=namespace,
+        predicate=predicate,
+        value=value
     ).count() > 0
+
 
 def obj_for_tag(*args):
     namespace, predicate, value = parse_machinetag(*args)
     found = list(MachineTaggedItem.objects.filter(
-        namespace = namespace,
-        predicate = predicate,
-        value = value
+        namespace=namespace,
+        predicate=predicate,
+        value=value
     ))
     if len(found) > 0:
         return found[0].content_object
     else:
         return False
 
+
 def add_machinetag(obj, namespace_or_fulltag, predicate=None, value=None):
     if predicate:
-        assert value, 'If you provide a predicate you must also provide a value'
+        assert value, ('If you provide a predicate you must '
+                       'also provide a value')
         assert is_valid_part(namespace_or_fulltag), 'namespace must be valid'
         assert is_valid_part(predicate), 'predicate must be valid'
         namespace = namespace_or_fulltag
     else:
         namespace, predicate, value = parse_machinetag(namespace_or_fulltag)
     obj.machinetags.create(
-        namespace = namespace,
-        predicate = predicate,
-        value = value
+        namespace=namespace,
+        predicate=predicate,
+        value=value,
     )

1ad0e68c97a4e79b4f0541f413921e7e75f2b487
Bruno Renié <buburno@gmail.com>
2011-04-03 22:32:24 +0200

    Converted groupedselect to unix file type, updated the imports

--- djangopeople/groupedselect.py
+++ djangopeople/groupedselect.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.forms.util import flatatt
+from django.utils.encoding import smart_unicode
+from django.utils.html import escape
 from django.utils.safestring import mark_safe
 # From http://www.djangosnippets.org/snippets/200/
 
@@ -21,26 +24,24 @@ from django.utils.safestring import mark_safe
 #   <option value="yum">beer</option>
 # </select>
 
-class GroupedSelect(forms.Select): 
+class GroupedSelect(forms.Select):
     def render(self, name, value, attrs=None, choices=()):
-        from django.utils.html import escape
-        from django.forms.util import flatatt, smart_unicode
-        if value is None: value = '' 
-        final_attrs = self.build_attrs(attrs, name=name) 
-        output = [u'<select%s>' % flatatt(final_attrs)] 
+        if value is None: value = ''
+        final_attrs = self.build_attrs(attrs, name=name)
+        output = [u'<select%s>' % flatatt(final_attrs)]
         str_value = smart_unicode(value)
-        for group_label, group in self.choices: 
+        for group_label, group in self.choices:
             if group_label: # should belong to an optgroup
-                group_label = smart_unicode(group_label) 
-                output.append(u'<optgroup label="%s">' % escape(group_label)) 
+                group_label = smart_unicode(group_label)
+                output.append(u'<optgroup label="%s">' % escape(group_label))
             for k, v in group:
                 option_value = smart_unicode(k)
-                option_label = smart_unicode(v) 
+                option_label = smart_unicode(v)
                 selected_html = (option_value == str_value) and u' selected="selected"' or ''
-                output.append(u'<option value="%s"%s>%s</option>' % (escape(option_value), selected_html, escape(option_label))) 
+                output.append(u'<option value="%s"%s>%s</option>' % (escape(option_value), selected_html, escape(option_label)))
             if group_label:
-                output.append(u'</optgroup>') 
-        output.append(u'</select>') 
+                output.append(u'</optgroup>')
+        output.append(u'</select>')
         return mark_safe(u'\n'.join(output))
 
 # field for grouped choices, handles cleaning of funky choice tuple
@@ -48,7 +49,7 @@ class GroupedChoiceField(forms.ChoiceField):
     def __init__(self, choices=(), required=True, widget=GroupedSelect, label=None, initial=None, help_text=None):
         super(forms.ChoiceField, self).__init__(required, widget, label, initial, help_text)
         self.choices = choices
-        
+
     def clean(self, value):
         """
         Validates that the input is in self.choices.

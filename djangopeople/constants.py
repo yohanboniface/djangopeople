6e6dcc964d875f30f8e03ecf312a9033a2ab1d51
Dan Fairs <dan@fezconsulting.com>
2011-04-11 21:33:47 +0100

    Mechanical search-and-replace of /static/ -> {{ STATIC_URL }}
    Note that there are still some occurrences of a hardcoded /static/ URL in javascript files

--- djangopeople/constants.py
+++ djangopeople/constants.py
@@ -1,25 +1,27 @@
+from django.conf import settings
+
 SERVICES = (
     # shortname, name, icon
-    ('flickr', 'Flickr', '/static/img/services/flickr.png'),
-    ('delicious', 'del.icio.us', '/static/img/services/delicious.png'),
-    ('magnolia', 'Ma.gnolia.com', '/static/img/services/magnolia.png'),
-    ('twitter', 'Twitter', '/static/img/services/twitter.png'),
-    ('facebook', 'Facebook', '/static/img/services/facebook.png'),
-    ('linkedin', 'LinkedIn', '/static/img/services/linkedin.png'),
-    ('pownce', 'Pownce', '/static/img/services/pownce.png'),
-    ('djangosnippets', 'djangosnippets.org', '/static/img/services/django.png'),
-    ('djangosites', 'DjangoSites.org', '/static/img/services/django.png'),
+    ('flickr', 'Flickr', settings.STATIC_URL + 'img/services/flickr.png'),
+    ('delicious', 'del.icio.us', settings.STATIC_URL + 'img/services/delicious.png'),
+    ('magnolia', 'Ma.gnolia.com', settings.STATIC_URL + 'img/services/magnolia.png'),
+    ('twitter', 'Twitter', settings.STATIC_URL + 'img/services/twitter.png'),
+    ('facebook', 'Facebook', settings.STATIC_URL + 'img/services/facebook.png'),
+    ('linkedin', 'LinkedIn', settings.STATIC_URL + 'img/services/linkedin.png'),
+    ('pownce', 'Pownce', settings.STATIC_URL + 'img/services/pownce.png'),
+    ('djangosnippets', 'djangosnippets.org', settings.STATIC_URL + 'img/services/django.png'),
+    ('djangosites', 'DjangoSites.org', settings.STATIC_URL + 'img/services/django.png'),
 )
 SERVICES_DICT = dict([(r[0], r) for r in SERVICES])
 
 IMPROVIDERS = (
     # shortname, name, icon
-    ('aim', 'AIM', '/static/img/improviders/aim.png'),
-    ('yim', 'Y!IM', '/static/img/improviders/yim.png'),
-    ('gtalk', 'GTalk', '/static/img/improviders/gtalk.png'),
-    ('msn', 'MSN', '/static/img/improviders/msn.png'),
-    ('jabber', 'Jabber', '/static/img/improviders/jabber.png'),
-    ('django', '#django IRC', '/static/img/services/django.png'),
+    ('aim', 'AIM', settings.STATIC_URL + 'img/improviders/aim.png'),
+    ('yim', 'Y!IM', settings.STATIC_URL + 'img/improviders/yim.png'),
+    ('gtalk', 'GTalk', settings.STATIC_URL + 'img/improviders/gtalk.png'),
+    ('msn', 'MSN', settings.STATIC_URL + 'img/improviders/msn.png'),
+    ('jabber', 'Jabber', settings.STATIC_URL + 'img/improviders/jabber.png'),
+    ('django', '#django IRC', settings.STATIC_URL + 'img/services/django.png'),
 )
 IMPROVIDERS_DICT = dict([(r[0], r) for r in IMPROVIDERS])
 

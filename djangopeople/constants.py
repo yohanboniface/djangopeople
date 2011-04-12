1fd868e0d326d256d8ad883fa3c80e9b9f67fae7
Dan Fairs <dan@fezconsulting.com>
2011-04-12 19:05:46 +0100

    No need to add STATIC_URL here - do it when we display in a template.

--- djangopeople/constants.py
+++ djangopeople/constants.py
@@ -1,27 +1,25 @@
-from django.conf import settings
-
 SERVICES = (
     # shortname, name, icon
-    ('flickr', 'Flickr', settings.STATIC_URL + 'img/services/flickr.png'),
-    ('delicious', 'del.icio.us', settings.STATIC_URL + 'img/services/delicious.png'),
-    ('magnolia', 'Ma.gnolia.com', settings.STATIC_URL + 'img/services/magnolia.png'),
-    ('twitter', 'Twitter', settings.STATIC_URL + 'img/services/twitter.png'),
-    ('facebook', 'Facebook', settings.STATIC_URL + 'img/services/facebook.png'),
-    ('linkedin', 'LinkedIn', settings.STATIC_URL + 'img/services/linkedin.png'),
-    ('pownce', 'Pownce', settings.STATIC_URL + 'img/services/pownce.png'),
-    ('djangosnippets', 'djangosnippets.org', settings.STATIC_URL + 'img/services/django.png'),
-    ('djangosites', 'DjangoSites.org', settings.STATIC_URL + 'img/services/django.png'),
+    ('flickr', 'Flickr', 'img/services/flickr.png'),
+    ('delicious', 'del.icio.us', 'img/services/delicious.png'),
+    ('magnolia', 'Ma.gnolia.com', 'img/services/magnolia.png'),
+    ('twitter', 'Twitter', 'img/services/twitter.png'),
+    ('facebook', 'Facebook', 'img/services/facebook.png'),
+    ('linkedin', 'LinkedIn', 'img/services/linkedin.png'),
+    ('pownce', 'Pownce', 'img/services/pownce.png'),
+    ('djangosnippets', 'djangosnippets.org', 'img/services/django.png'),
+    ('djangosites', 'DjangoSites.org', 'img/services/django.png'),
 )
 SERVICES_DICT = dict([(r[0], r) for r in SERVICES])
 
 IMPROVIDERS = (
     # shortname, name, icon
-    ('aim', 'AIM', settings.STATIC_URL + 'img/improviders/aim.png'),
-    ('yim', 'Y!IM', settings.STATIC_URL + 'img/improviders/yim.png'),
-    ('gtalk', 'GTalk', settings.STATIC_URL + 'img/improviders/gtalk.png'),
-    ('msn', 'MSN', settings.STATIC_URL + 'img/improviders/msn.png'),
-    ('jabber', 'Jabber', settings.STATIC_URL + 'img/improviders/jabber.png'),
-    ('django', '#django IRC', settings.STATIC_URL + 'img/services/django.png'),
+    ('aim', 'AIM', 'img/improviders/aim.png'),
+    ('yim', 'Y!IM', 'img/improviders/yim.png'),
+    ('gtalk', 'GTalk', 'img/improviders/gtalk.png'),
+    ('msn', 'MSN', 'img/improviders/msn.png'),
+    ('jabber', 'Jabber', 'img/improviders/jabber.png'),
+    ('django', '#django IRC', 'img/services/django.png'),
 )
 IMPROVIDERS_DICT = dict([(r[0], r) for r in IMPROVIDERS])
 

19b64d0e8dc1c878ba5592fcde7c7186e3c537de
Filip Jukić <no.idea13@gmail.com>
2011-04-13 19:06:09 +0200

    Switched to class-based template loaders

--- default_settings.py
+++ default_settings.py
@@ -64,8 +64,8 @@ API_PASSWORD = 'API-PASSWORD-GOES-HERE'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (

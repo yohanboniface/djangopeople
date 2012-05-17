b86bcfd1f1762cbb956b3eb42b515107249cb66e
Bruno Renié <buburno@gmail.com>
2012-05-17 23:26:54 +0200

    Disable querystring auth for s3

--- production_settings.py
+++ production_settings.py
@@ -13,6 +13,7 @@ STATICFILES_STORAGE = 's3storage.S3HashedFilesStorage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME', '')
+AWS_QUERYSTRING_AUTH = False
 
 STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 

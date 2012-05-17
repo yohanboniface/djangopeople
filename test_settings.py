c5fd37531f54346b4ec40cbcd2a95ea392dce321
Bruno Renié <buburno@gmail.com>
2012-05-17 15:38:56 +0200

    Updated travis config to work (hopefully)

---
+++ test_settings.py
@@ -0,0 +1,9 @@
+from default_settings import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangopeople',
+        'USER': 'postgres',
+    },
+}

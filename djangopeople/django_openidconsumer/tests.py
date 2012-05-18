f7b6195dfce88be5eb7017767107390b0797ec9e
Bruno Renié <buburno@gmail.com>
2012-05-18 16:21:31 +0200

    Added mock to avoid making a network call during tests

--- djangopeople/django_openidconsumer/tests.py
+++ djangopeople/django_openidconsumer/tests.py
@@ -1,9 +1,12 @@
+from mock import patch
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 
 class OpenIDTests(TestCase):
-    def test_openid_begin(self):
+    @patch('openid.consumer.consumer.Consumer')
+    def test_openid_begin(self, consumer):
         """
         OpenID initial negociation
         """
@@ -13,7 +16,12 @@ class OpenIDTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
         data = {'openid_url': 'http://bruno.renie.fr'}
+
+        auth = consumer.return_value.begin
+        auth.return_value.redirectURL.return_value = 'http://www.myopenid.com/server'
+
         response = self.client.post(url, data)
+        auth.assert_called_with('http://bruno.renie.fr')
         self.assertEqual(response.status_code, 302)
         self.assertTrue(
             response['Location'].startswith('http://www.myopenid.com/server')

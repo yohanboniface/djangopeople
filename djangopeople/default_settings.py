eb715f733e57be037528046c79a22e5a02c9a682
Bruno Renié <buburno@gmail.com>
2012-06-06 16:13:04 +0200

    Added MessageMiddleware

--- djangopeople/default_settings.py
+++ djangopeople/default_settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'djangopeople.django_openidconsumer.middleware.OpenIDMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'djangopeople.djangopeople.middleware.NoDoubleSlashes',
